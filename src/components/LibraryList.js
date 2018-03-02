b2ba01d327bbb4a122fe0dae3bbc9e4cfa5ddb39
BoomTech <boomtechny@gmail.com>
2018-03-01 22:42:15 -0500

    Configured passing data to LibraryList component

--- src/components/LibraryList.js
+++ src/components/LibraryList.js
@@ -1,13 +1,17 @@
-import React, {Component} from 'react'; 
+import React, { Component } from 'react'; 
 import { connect } from 'react-redux'; 
 
 
 class LibraryList extends Component{
   render(){
+    console.log(this.props); 
     return; 
   }
 }
 
-export defalt connect()(LibraryList); 
+const mapStateToProps = state => {
+  return { libraries: state.libraries}; 
+}
+export default connect(mapStateToProps)(LibraryList); 
 
 
--- src/reducers/LibraryReducer.js
+++ src/reducers/LibraryReducer.js
@@ -1,2 +1,2 @@
 import data from './LibraryList.json'; 
-export default () => []; 
+export default () => data; 
